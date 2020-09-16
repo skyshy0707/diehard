70d7dd9647bc2e640b11b84e6ca7085e17cb02b9
skyshy0707 <alekszskyshy@gmail.com>
2020-09-16 22:51:41 +0600

    change secret dir of parameters

--- diehard/settings.py
+++ diehard/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from decouple import config
+'''from decouple import config'''
+import env as e
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = e.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG =  config('DEBUG', cast=bool)
+DEBUG =  e.DEBUG
 
 ALLOWED_HOSTS = ['*']
 
