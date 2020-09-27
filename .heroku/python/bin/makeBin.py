4983d4dc8e1432805c421920dce4b60cc70f5802
skyshy0707 <alekszskyshy@gmail.com>
2020-09-27 19:03:16 +0600

    set executable to exe

--- .heroku/python/bin/makeBin.py
+++ .heroku/python/bin/makeBin.py
@@ -9,11 +9,11 @@ Created on Thu Jul  9 17:37:18 2020
 import subprocess
 import os
 '''from diehard.settings import BASE_DIR'''
-
+from sys import executable
 def main(inpPath, outPath):
 	'''init_path = os.path.join(BASE_DIR, 'inputData')
 	os.chdir(init_path)'''
-	p = subprocess.Popen(['ASC2BIN.EXE'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text = True)
+	p = subprocess.Popen([executable, 'ASC2BIN.EXE'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text = True)
 	p.communicate(input='\n\n\n' + inpPath + '\n' + outPath)
 	
 
